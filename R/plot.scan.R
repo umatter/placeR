695584caea709655af1b1d0ae374d9263fc4ec9d
umatter <umatter@protonmail.com>
2019-03-27 11:01:24 +0100

    resolve issue #6: extend plot.scan with funcitonality to plot map polygon instead of google map.

--- R/plot.scan.R
+++ R/plot.scan.R
@@ -1,7 +1,7 @@
 ##' Plot Method For Scan Objects
 ##'
 ##' Takes an object of class scan and plots the locations and search grid to a map
-##' @usage plot.scan(x, ggmap.zoom=7, ncircles=1, col.grid="white", col.loc="#e55562", col.area="#6b3a70", col.search="#4db0d9", col.circles="#4db0d9" )
+##' @usage plot.scan(x, ggmap.zoom=7, ncircles=1, col.grid="white", col.loc="#e55562", col.area="#6b3a70", col.search="#4db0d9", col.circles="#4db0d9", poly.map = NULL )
 ##' @param x object of class scan
 ##' @param ggmap.zoom integer, zoom parameter for ggmap output
 ##' @param ncircles integer, the number of circles to be plotted (centered around search area, defaults to 1)
@@ -10,62 +10,127 @@
 ##' @param col.area character, the color of the rectangle indicating the search area (in hexadecimal or name of R-color; default: "#6b3a70")
 ##' @param col.search character, the color of the search area center (in hexadecimal or name of R-color; default: "#4db0d9")
 ##' @param col.search character, the color of the circles indicating the search radius (in hexadecimal or name of R-color; default: "#4db0d9")
+##' @param poly.map spatialPolygonDataframe (optional), map data as basis for the plot. If empty (NULL), the function plots a google map as basis.
 ##' @return a ggplot object
 ##' @author Ulrich Matter <umatter@protonmail.com>
 ##' @export
 ##' @import ggmap
+##' @importFrom rgeos gCentroid
+##' @importFrom ggthemes theme_map
 
 
 # definition of the plot function
 plot.scan <-
-  function(x, ggmap.zoom=7, ncircles=1, col.grid="white", col.loc="#e55562", col.area="#6b3a70", col.search="#4db0d9", col.circles="#4db0d9" ) {
+  function(x,
+           ggmap.zoom=7,
+           ncircles=1,
+           col.grid="gray",
+           col.loc="#e55562",
+           col.area="#6b3a70",
+           col.search="#4db0d9",
+           col.circles="#4db0d9",
+           poly.map = NULL) {
 
-    if(class(x)!="scan") {
+    if (class(x)!="scan") {
 
       cat(paste("Error: ",bquote(x), " is not an object of class scan.\n", sep=""))
     } else {
 
-      # register api key
-      register_google(api.key)
+      if (is.null(poly.map) ){
+        # register api key
+        register_google(api.key)
 
-      # get necessary data
-      data <- x@locations@illustration # data related to the search input/the area searched
-      center <- data$map.center
-      grid.vline <- data$grid.vline
-      grid.hline <- data$grid.hline
-      area.edges <- data$area.edges
-      search.points <- data$search.points
-      circles <- data$circles
+        # get necessary data
+        data <- x@locations@illustration # data related to the search input/the area searched
+        center <- data$map.center
+        grid.vline <- data$grid.vline
+        grid.hline <- data$grid.hline
+        area.edges <- data$area.edges
+        search.points <- data$search.points
+        circles <- data$circles
 
-      search.data <- x@search.results # data related to the scan-search results (transformed response from the api)
+        search.data <- x@search.results # data related to the scan-search results (transformed response from the api)
 
-      # plot the scan object with ggmap/ggplot
-      center_map <- get_googlemap(center, zoom=ggmap.zoom, scale=2, color="bw")
+        # plot the scan object with ggmap/ggplot
+        center_map <- get_googlemap(center, zoom=ggmap.zoom, scale=2, color="bw")
 
-      p <-  ggmap(center_map) +
-        geom_vline(aes(xintercept = x), data=grid.vline, colour=col.grid, alpha=.5) +
-        geom_hline(aes(yintercept = y), data=grid.hline, colour=col.grid, alpha=.5)+
-        geom_point(aes(x = lng, y = lat), data = search.data, shape=1, size = 1, colour = col.loc)+ # add the search results in an extra layer
-        geom_point(aes(x = x, y = y), data = area.edges, shape=3, size = 3, colour = col.area)+
-        geom_point(aes(x = x, y = y), data = search.points, shape=3, size=3, colour=col.search)+
-        geom_line(aes(x=x, y=y), data= area.edges[1:2,], colour=col.area)+
-        geom_line(aes(x=x, y=y), data= area.edges[c(1,3),], colour=col.area)+
-        geom_line(aes(x=x, y=y), data= area.edges[c(2,4),], colour=col.area)+
-        geom_line(aes(x=x, y=y), data= area.edges[3:4,], colour=col.area)
+        p <-  ggmap(center_map) +
+          geom_vline(aes(xintercept = x), data=grid.vline, colour=col.grid, alpha=.5) +
+          geom_hline(aes(yintercept = y), data=grid.hline, colour=col.grid, alpha=.5)+
+          geom_point(aes(x = lng, y = lat), data = search.data, shape=1, size = 1, colour = col.loc)+ # add the search results in an extra layer
+          geom_point(aes(x = x, y = y), data = area.edges, shape=3, size = 3, colour = col.area)+
+          geom_point(aes(x = x, y = y), data = search.points, shape=3, size=3, colour=col.search)+
+          geom_line(aes(x=x, y=y), data= area.edges[1:2,], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[c(1,3),], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[c(2,4),], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[3:4,], colour=col.area)
 
-      if (ncircles>0){
+        if (ncircles>0){
 
-        n <- ifelse(ncircles>length(circles), length(circles), ncircles)
+          n <- ifelse(ncircles>length(circles), length(circles), ncircles)
 
-        for (i in 1:n) {
+          for (i in 1:n) {
 
-          p <- p + geom_path( aes(x,y), data=circles[[i]], colour=col.circles)
+            p <- p + geom_path( aes(x,y), data=circles[[i]], colour=col.circles)
 
+          }
         }
+        return(p)
+
+      } else {
+
+
+        # get necessary data
+        data <- x@locations@illustration # data related to the search input/the area searched
+        center <- coordinates(gCentroid(poly.map))
+        grid.vline <- data$grid.vline
+        grid.hline <- data$grid.hline
+        area.edges <- data$area.edges
+        search.points <- data$search.points
+        circles <- data$circles
+
+        search.data <- x@search.results # data related to the scan-search results (transformed response from the api)
+
+        # prepare map data for plot with ggplot
+        mapdata <- fortify(poly.map)
+
+        # plot the scan object with ggplot
+
+        p <-
+          ggplot(mapdata, aes(x=long, y=lat)) +
+          geom_map(map=mapdata, aes(map_id=id)) +
+          theme_map() +
+          coord_equal() +
+          geom_vline(aes(xintercept = x), data=grid.vline, colour=col.grid, alpha=.5) +
+          geom_hline(aes(yintercept = y), data=grid.hline, colour=col.grid, alpha=.5)+
+          geom_point(aes(x = lng, y = lat), data = search.data, shape=1, size = 1, colour = col.loc)+ # add the search results in an extra layer
+          geom_point(aes(x = x, y = y), data = area.edges, shape=3, size = 3, colour = col.area)+
+          geom_point(aes(x = x, y = y), data = search.points, shape=3, size=3, colour=col.search)+
+          geom_line(aes(x=x, y=y), data= area.edges[1:2,], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[c(1,3),], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[c(2,4),], colour=col.area)+
+          geom_line(aes(x=x, y=y), data= area.edges[3:4,], colour=col.area)
+
+        if (ncircles>0){
+
+          n <- ifelse(ncircles>length(circles), length(circles), ncircles)
+
+          for (i in 1:n) {
+
+            p <- p + geom_path( aes(x,y), data=circles[[i]], colour=col.circles)
+
+          }
+        }
+        return(p)
       }
-      return(p)
+
     }
+
+
   }
+
+
+
 
 
 # set the method:
